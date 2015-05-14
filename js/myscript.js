07befd5b280f5d27940c380b5f6c72a31d5ebe7a
Daniel Abdel-Nour <daniel.a.nour@gmail.com>
2015-05-14 14:49:17 +0930

    working

--- js/myscript.js
+++ js/myscript.js
@@ -1,94 +1,116 @@
-var map = L.map('map').setView([-34.92866, 138.59863], 13);
-
-function style(feature) {
-    return {
-        fillColor: 'green', //TODO
-        fillOpacity: 0.4,
-        weight: 2,
-        opacity: 1,
-        color: 'white',  
-        dashArray: '3'
-    };
-}
-
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera) {
-        layer.bringToFront();
-    }
-}
-
-var geojson;
-
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-}
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight
-    });
-}
+var map = L.map('map').setView([-34.92866, 138.59863], 10);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
     maxZoom: 14,
     id: 'examples.map-i875mjb7'
 }).addTo(map);
 
-geojson = L.geoJson(lga, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
+var files = [
+"res/Adelaide Hills.csv",
+"res/Adelaide.csv",
+"res/Burnside.csv",
+"res/Campbelltown.csv",
+"res/Charles Sturt.csv",
+"res/Gawler.csv",
+"res/Holdfast Bay.csv",
+"res/Marion.csv",
+"res/Mitcham.csv",
+"res/Norwood Payneham St Peters.csv",
+"res/Onkaparinga.csv",
+"res/Playford.csv",
+"res/Port Adelaide Enfield.csv",
+"res/Prospect.csv",
+"res/Salisbury.csv",
+"res/Tea Tree Gully.csv",
+"res/Unley.csv",
+"res/Walkerville.csv",
+"res/West Torrens.csv"];
+
+//[LGA][OFFENCE TYPE][YEAR]
+
+var lgaData = [];
+var count = 0;
+var geojson;
 
 
-file = "http://127.0.0.1:62741/res/Adelaide.csv";
+for (var i = 0; i < files.length; i++) {
+    Papa.parse(files[i], {
+        download: true,
+        complete: function (results) {
+            lgaData.push(results.data);
+            count++;
+            if (count == files.length) {
+                console.log(lgaData);
+                addLGAs(lgaData);
+            }
+        }
+    });
+}
 
-Papa.parse(file, {
-    download: true,
-    complete: function (results){
-        console.log(results.data[2]);
+function addLGAs(data) {
+    lgaData = data;
+
+    function getColor(name) {
+        for (var i = 0; i < files.length; i++) {
+            if (lgaData[i][0][0] == name) {
+                return nameToColor(lgaData[i][1][5]);
+            }
+        }
     }
-});
 
-//Controls
-var info = L.control();
+    function nameToColor(d) {
+        return d > 60 ? '#990000' :
+            d > 20 ? '#d7301f' :
+            d > 13 ? '#ef6548' :
+            d > 10 ? '#fc8d59' :
+            d > 7 ? '#fdbb84' :
+            d > 5 ? '#fdd49e' :
+            d > 3 ? '#fee8c8' :
+            '#fff7ec';
+    }
 
-info.onAdd = function (map) {
-    this.div = L.DomUtil.create('div', 'info');
-    this.div.innerHTML =
-        '<h4>Select Offence Type</h4>' +
-        '<input type ="checkbox" id="assault">Assault<br>'+
-        '<input type ="checkbox" id="robbery">Robbery<br>'+
-        '<input type ="checkbox" id="drugs">Drug related';
-    return this.div;
-};
+    function style(feature) {
+        return {
+            fillColor: getColor(feature.properties.ABBNAME),
+            fillOpacity: 0.7,
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3'
+        };
+    }
 
+    function highlightFeature(e) {
+        var layer = e.target;
 
-info.addTo(map);
+        layer.setStyle({
+            weight: 4,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.8
+        });
 
+        if (!L.Browser.ie && !L.Browser.opera) {
+            layer.bringToFront();
+        }
+    }
 
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
+    }
 
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+        });
+    }
 
+    geojson = L.geoJson(lga, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
 
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
